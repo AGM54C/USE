13caa86f93b4c9e364064144110315b834c69c69
agm54c <agm54c@gmail.com>
2025-05-29 20:48:56 +0800

    初步实现定向飞行（逻辑有错误但是没bug）

--- src/main/java/com/example1/demo2/service/IPlanetAccessService.java
+++ src/main/java/com/example1/demo2/service/IPlanetAccessService.java
@@ -1,9 +1,13 @@
 package com.example1.demo2.service;
 
 import com.example1.demo2.pojo.KnowledgePlanet;
-
+import com.example1.demo2.pojo.dto.KnowledgePlanetDto;
+import com.example1.demo2.pojo.dto.UserDto;
+import jakarta.validation.Valid;
 public interface IPlanetAccessService {
 
+    //按标题查找知识星球
+    KnowledgePlanet findByTitle(String title);
     //随机查找知识星球
     KnowledgePlanet findRandomPlanet();
 }
--- src/main/java/com/example1/demo2/service/impl/PlanetAccessService.java
+++ src/main/java/com/example1/demo2/service/impl/PlanetAccessService.java
@@ -1,12 +1,25 @@
 package com.example1.demo2.service.impl;
 
+import com.example1.demo2.mapper.PlanetAccessMapper;
+import com.example1.demo2.mapper.PlanetMapper;
 import com.example1.demo2.pojo.KnowledgePlanet;
 import com.example1.demo2.service.IPlanetAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PlanetAccessService implements IPlanetAccessService {
+
+    @Autowired
+    private PlanetAccessMapper planetAccessMapper;
+
+    @Override
+    public KnowledgePlanet findByTitle(String title) {
+        return planetAccessMapper.findByTitle(title);
+    }
 
     @Override
     public KnowledgePlanet findRandomPlanet() {
-        return null;
+        return planetAccessMapper.findRandomPlanet();
     }
 }
