43a38d9c4b6ebeb262caa7cd96b337dc3aec69c2
agm54c <agm54c@gmail.com>
2025-06-09 00:47:56 +0800

    nickname

--- src/main/java/com/example1/demo2/pojo/GalaxyAdministrator.java
+++ src/main/java/com/example1/demo2/pojo/GalaxyAdministrator.java
@@ -32,6 +32,12 @@ public class GalaxyAdministrator {
     private User user;
 
     /**
+     * 管理员昵称
+     */
+    @Column(name = "nickname", nullable = false, length = 50)
+    private String nickname;
+
+    /**
      * 角色类型(0 创建者 1 其他管理员)
      */
     @Column(name = "role_type", nullable = false, columnDefinition = "tinyint default 0")
@@ -104,6 +110,14 @@ public class GalaxyAdministrator {
         this.user = user;
     }
 
+    public String getNickname() {
+        return user.getNickname();
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = this.user.getNickname();
+    }
+
     public Integer getRoleType() {
         return roleType;
     }
@@ -150,6 +164,7 @@ public class GalaxyAdministrator {
                 "adminId=" + adminId +
                 ", knowledgeGalaxy=" + knowledgeGalaxy +
                 ", user=" + user +
+                ", nickname='" + nickname + '\'' +
                 ", roleType=" + roleType +
                 ", permissions='" + permissions + '\'' +
                 ", appointedBy=" + appointedBy +
